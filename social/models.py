fc858e85d96d9d308e81b516c2249649e5a1fd33
Oyku Gencbay <your-email@example.com>
2026-06-12 21:01:23 +0300

    Fix Cloudinary and static file storage

--- social/models.py
+++ social/models.py
@@ -24,7 +24,7 @@ class AddCode(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     used = models.BooleanField(default=False)
     def is_expired(self):
-        return timezone.now() > self.created_at + timedelta(minutes=1)
+        return timezone.now() > self.created_at + timedelta(minutes=10)
     @staticmethod
     def generate_code():
         return str(random.randint(100000, 999999))
