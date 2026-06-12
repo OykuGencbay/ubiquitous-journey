e14affd1f8fa25644c04d54a0d76fe195a4ced6d
Oyku Gencbay <your-email@example.com>
2026-06-12 21:05:04 +0300

    Fix Cloudinary and static file storage

--- social/admin.py
+++ social/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Profile, Connection, AddCode, Post, Comment
+from .models import ForumTopic, ForumReply
 admin.site.register(Profile)
 admin.site.register(Connection)
 admin.site.register(AddCode)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(ForumTopic)
+admin.site.register(ForumReply)
